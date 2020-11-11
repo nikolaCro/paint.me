6692201d63d58bf3fc90749058f5a57d12844aa9
nikolalucic <nikola.lucic.zg@gmail.com>
2020-11-11 22:38:19 +0100

    ...

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const colorPicker = document.querySelector('.picker');
-const mask = document.querySelectorAll('path');
-const colorPickerLabel = document.querySelector('#label');
+const colorPickerBrush = document.querySelector('.picker-brush');
 
-const changeColor = () => {
+const mask = document.querySelectorAll('.mask path');
+const windowsMask = document.querySelectorAll('.windows-mask path');
+
+const colorPickerLabel = document.querySelector('#label');
+const colorPickerLabelBrush = document.querySelector('#brush');
+
+
+// Walls
+
+const changeColorWalls = () => {
     for (let i = 0; i < mask.length; i++) {
         mask[i].style.fill = colorPicker.value;
+        colorPickerBrush.value = colorPicker.value;
     }
-    colorPickerLabel.style.background = colorPicker.value;
 };
 
-colorPicker.addEventListener('input', changeColor);
+
+// Windows
+
+const changeColorWindows = () => {
+    for (let i = 0; i < windowsMask.length; i++) {
+        windowsMask[i].style.fill = colorPickerBrush.value;
+    }
+};
+
+
+// Events
+
+colorPicker.addEventListener('input', changeColorWalls);
+colorPickerBrush.addEventListener('input', changeColorWindows);
